083776b42574418c4d3d8175859bf94c67b36d1d
Moritz Salla <moritz@buildinamsterdam.com>
2025-01-29 17:18:26 +0100

    Add Leva UI for enhanced control panel; customize theme settings for improved layout and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { Leva } from "leva";
 import "./App.css";
 import { Scene } from "./components/Scene";
 
 export const App = () => {
-	return (
-		<Scene />
-	);
+  return (
+    <>
+      <Leva
+        theme={{
+          sizes: {
+            rootWidth: "340px",
+            // controlWidth: "100px",
+            // rowHeight: "24px",
+            titleBarHeight: "24px",
+          },
+          fonts: {
+            mono: "monospace",
+            sans: "system-ui",
+          },
+        }}
+      />
+      <Scene />
+    </>
+  );
 };
